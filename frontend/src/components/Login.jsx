ebfcaf5253ee069f11abf3f7c4b4c63b19519060
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-04-02 15:22:00 +0200

    changed some styles for the login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -58,10 +58,10 @@ const Login = ({ toggleLogin }) => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl text-[#607d8b] font-extrabold">
           Sign in to your account
         </h2>
-        <p className="mt-2 text-center text-sm text-gray-600 max-w">
+        <p className="mt-2 text-center text-sm text-[#607d8b] max-w">
           Or <span className="w-1"></span>
           <a href="#" className="font-medium text-blue-600 hover:text-blue-500" onClick={toggleLogin}>
             create an account
@@ -78,7 +78,7 @@ const Login = ({ toggleLogin }) => {
               </label>
               <div className="mt-1">
                 <input id="loginIdentifier" name="loginIdentifier" type="text" required
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#607d8b] focus:border-[#607d8b] focus:z-10 sm:text-sm"
                   placeholder="Email, Username" onChange={handleIdentifierChange} />
               </div>
             </div>
@@ -89,7 +89,7 @@ const Login = ({ toggleLogin }) => {
               </label>
               <div className="mt-1">
                 <input id="password" name="password" type="password" autoComplete="current-password" required
-                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#607d8b] focus:border-[#607d8b] focus:z-10 sm:text-sm"
                   placeholder="Enter your password" onChange={handlePasswordChange} />
               </div>
             </div>
@@ -97,14 +97,14 @@ const Login = ({ toggleLogin }) => {
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <input id="remember_me" name="remember_me" type="checkbox"
-                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" onChange={handleRememberChange} />
+                  className="h-4 w-4 text-[#607d8b] focus:ring-[#607d8b] border-[#607d8b] rounded" onChange={handleRememberChange} />
                 <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
                   Remember me
                 </label>
               </div>
 
               <div className="text-sm">
-                <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+                <a href="#" className="font-medium text-[#607d8b] hover:text-[#607d8b]">
                   Forgot your password?
                 </a>
               </div>
@@ -112,7 +112,7 @@ const Login = ({ toggleLogin }) => {
 
             <div>
               <button type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                className="group relative w-full flex justify-center py-2 px-4 border border-2 border-[#607d8b] hover:border-[#607d8b] text-sm font-medium text-[#607d8b] hover:text-white bg-white hover:bg-[#607d8b] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#607d8b]">
                 Sign in
               </button>
             </div>
