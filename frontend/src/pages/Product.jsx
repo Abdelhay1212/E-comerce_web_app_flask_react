f7b46fd4e2a492e29b4f9674cddd6b6376a3307e
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-04-05 00:08:51 +0000

    created the products page and its functionalities

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,106 @@
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
 import { useParams } from "react-router-dom"
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+import { Helmet } from 'react-helmet'
 
-const Product = () => {
+const Product = ({ addToCart, itemsInfo, getCartItems, cartItems, deleteCartItem }) => {
+
+  Product.propTypes = {
+    addToCart: PropTypes.func.isRequired,
+    itemsInfo: PropTypes.object.isRequired,
+    getCartItems: PropTypes.func.isRequired,
+    cartItems: PropTypes.array.isRequired,
+    deleteCartItem: PropTypes.func.isRequired
+  }
+
   const { productId } = useParams()
+  const [quantity, setQuantity] = useState(1)
+  const [product, setProduct] = useState({})
+
+  async function getProduct(productId) {
+    try {
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+
+      const response = await fetch(`http://localhost:5000/api/v1/views/product/${productId}`, options)
+      const data = await response.json()
+      if (response.ok) {
+        setProduct(data)
+        return
+      }
+      if (data.error) {
+        alert(data.error)
+        return
+      }
+      alert("An error occurred")
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getProduct(productId)
+  }, [productId])
+
   return (
-    <>
-      <h1>Product Page {productId}</h1>
-    </>
+    <div className="bg-gray-200">
+
+      <Helmet>
+        <title>Product</title>
+      </Helmet>
+
+      <NavBar
+        itemsInfo={itemsInfo}
+        getCartItems={getCartItems}
+        cartItems={cartItems}
+        deleteCartItem={deleteCartItem}
+      />
+
+      <div className="bg-gray-200">
+        <div className="max-w-6xl bg-white pb-8 pt-20 mx-auto sm:px-6 lg:px-8">
+          {product &&
+            <div className="flex flex-col md:flex-row -mx-4">
+              <div className="md:flex-1 px-4">
+                <div className="h-[460px] rounded-lg mb-4">
+                  <img className="w-full h-full object-cover" src={`http://localhost:5000/images/${product.image}`} alt="Product Image" />
+                </div>
+              </div>
+              <div className="md:flex-1 px-4">
+                <div className="flex justify-between items-center">
+                  <span className="text-sm text-gray-500">{`Home / ${product.categories} / ${product.title}`}</span>
+                  <div className="flex justify-start items-center gap-2">
+                    <span className="w-6 h-6 border border-[#607d8b] block text-[#607d8b] hover:bg-[#607d8b] text-xl cursor-pointer hover:text-white flex justify-center items-center">&#60;</span>
+                    <span className="w-6 h-6 border border-[#607d8b] block text-[#607d8b] hover:bg-[#607d8b] text-xl cursor-pointer hover:text-white flex justify-center items-center">&#62;</span>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <a href="" className="text-sm text-[#607d8b] hover:text-gray-500">{product.categories}</a>
+                </div>
+                <h2 className="capitalize text-[#607d8b] text-xl mt-3">{product.title}</h2>
+                <p className="font-bold text-gray-700 mt-2">${product.new_price}</p>
+                <p className="text-md text-gray-600 leading-loose mt-2">{product.description}</p>
+                <div className="flex flex-row justify-start items-center gap-5 mt-4">
+                  <div className="flex flex-row justify-start items-center">
+                    <button onClick={() => setQuantity(prevValue => Math.max(prevValue - 1, 1))} className="bg-white border border-gray-200 rounded-none text-gray-500 font-semibold px-4 py-2 hover:bg-white hover:border-gray-200 focus:outline-none">-</button>
+                    <input type="text" className="font-semibold text-gray-700 px-0 w-10 py-2 border-t border-b text-center" min={1} max={product.stock} value={quantity} readOnly />
+                    <button onClick={() => setQuantity(prevValue => Math.max(prevValue + 1, 1))} className="bg-white border border-gray-200 rounded-none text-gray-500 font-semibold px-4 py-2 hover:bg-white hover:border-gray-200 focus:outline-none">+</button>
+                  </div>
+                  <button onClick={() => addToCart(productId, quantity)} className="bg-white text-[#607d8b] border border-[#607d8b] hover:border-[#607d8b] hover:bg-[#607d8b] hover:text-white rounded-none px-20 py-2 focus:outline-none">Add to Cart</button>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
