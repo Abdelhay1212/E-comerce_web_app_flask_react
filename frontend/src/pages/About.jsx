8f87f3f07790993c3749cb77c803455485187001
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2024-04-05 00:05:18 +0000

    added the title

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,11 @@
-
+import { Helmet } from 'react-helmet'
 
 const About = () => {
   return (
     <>
+      <Helmet>
+        <title>About Us</title>
+      </Helmet>
       <h1>About Page</h1>
     </>
   );
